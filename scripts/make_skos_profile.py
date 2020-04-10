421e35c642ddf85c90398da3c24cb1a744241097
Nicholas Car <nicholas.car@csiro.au>
2020-04-10 22:54:16 +1000

    customising vocab title/defn

--- scripts/make_skos_profile.py
+++ scripts/make_skos_profile.py
@@ -12,6 +12,66 @@ g_skos.bind('owl', rdflib.Namespace("http://www.w3.org/2002/07/owl#"))
 g_skos.bind('sdo', rdflib.Namespace("https://schema.org/"))
 g_skos.bind('geof', rdflib.Namespace("http://linked.data.gov.au/def/geofeatures/"))
 
+# get Ontology as a ConceptScheme
+q = """
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+CONSTRUCT {
+    ?s a owl:Ontology , skos:ConceptScheme .
+    ?s ?p ?o .
+}
+WHERE {
+    ?s a owl:Ontology .
+    ?s ?p ?o .
+}
+"""
+g_skos += g.query(q)
+
+# add any class without a local subClassOf as a hasTopConcept
+q = """
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+PREFIX sdo: <https://schema.org/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+CONSTRUCT {
+    ?cs skos:hasTopConcept ?s .
+}
+WHERE {
+    ?cs a owl:Ontology .
+    ?s a owl:Class .
+
+    FILTER NOT EXISTS {?s rdfs:subClassOf ?z}
+}
+"""
+g_skos += g.query(q)
+
+# rename the vocab to add "Types"
+g_skos.remove((
+    rdflib.URIRef("http://linked.data.gov.au/def/geofeatures"),
+    rdflib.namespace.SKOS.prefLabel,
+    None
+))
+g_skos.add((
+    rdflib.URIRef("http://linked.data.gov.au/def/geofeatures"),
+    rdflib.namespace.SKOS.prefLabel,
+    rdflib.Literal("Geologic Feature Types", lang='en')
+))
+
+# replace the ont definition with this custom one for the vocab
+g_skos.remove((
+    rdflib.URIRef("http://linked.data.gov.au/def/geofeatures"),
+    rdflib.namespace.SKOS.definition,
+    None
+))
+g_skos.add((
+    rdflib.URIRef("http://linked.data.gov.au/def/geofeatures"),
+    rdflib.namespace.SKOS.definition,
+    rdflib.Literal(
+        "This vocabulary lists types of geological features relevant to the duties of the Geological Survey of "
+        "Queensland. It is automatically derived from GSQ's Geologic Features Ontology "
+        "(http://linked.data.gov.au/def/geofeatures)", lang='en')
+))
+
 # get Classes as Concepts
 q = """
 PREFIX owl: <http://www.w3.org/2002/07/owl#>
@@ -40,39 +100,6 @@ WHERE {
            dcterms:source ?source ;
            skos:notation ?n .
     }
-}
-"""
-g_skos += g.query(q)
-
-# get Ontology as a ConceptScheme
-q = """
-PREFIX owl: <http://www.w3.org/2002/07/owl#>
-PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-CONSTRUCT {
-    ?s a owl:Ontology , skos:ConceptScheme .
-    ?s ?p ?o .
-}
-WHERE {
-    ?s a owl:Ontology .
-    ?s ?p ?o .
-}
-"""
-g_skos += g.query(q)
-
-# add any class without a local subClassOf as a hasTopConcept
-q = """
-PREFIX owl: <http://www.w3.org/2002/07/owl#>
-PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-PREFIX sdo: <https://schema.org/>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-CONSTRUCT {
-    ?cs skos:hasTopConcept ?s .
-}
-WHERE {
-    ?cs a owl:Ontology .
-    ?s a owl:Class .
-    
-    FILTER NOT EXISTS {?s rdfs:subClassOf ?z}
 }
 """
 g_skos += g.query(q)
